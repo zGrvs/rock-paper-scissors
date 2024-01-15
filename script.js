f707823e62d09186b0237539851906aff0bdcd24
zGrvs <143911194+zGrvs@users.noreply.github.com>
2024-01-15 15:18:22 -0500

    Complete script modifications for UI

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const start_screen = document.querySelector(".start-screen");
+    const play_button = document.querySelector(".play");
+
+    const game_screen = document.querySelector(".game");
+    const result = document.querySelector(".result");
+    const score = document.querySelector(".score");
+
+    const end_screen = document.querySelector(".end-screen");
+    const end_text = document.querySelector(".end-text");
+    const again = document.querySelector(".again");
+
     const rock = document.querySelector(".rock");
     const paper = document.querySelector(".paper");
     const scissors = document.querySelector(".scissors");
 
+    
+    play_button.addEventListener("click", () => {
+        start_screen.classList.toggle("hidden");
+        start_screen.classList.toggle("visible");
+        game_screen.classList.toggle("hidden");
+        game_screen.classList.toggle("visible");
+    })
+    
     let playerScore = 0;
     let computerScore = 0;
 
@@ -26,34 +45,51 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function playRound(player, computer) {
-        let result;
         console.log(`Computer chose: ${computer}`);
 
         if (player === computer) {
-            result = "It's a tie!";
+            result.textContent = "It's a tie!";
         } else {
             switch (player) {
                 case "rock":
-                    computer === "paper" ? result = "You lose! Paper covers rock." : result = "You win! Rock smashes scissors.";
+                    computer === "paper" ? result.textContent = "You lose! Paper covers rock." : result.textContent = "You win! Rock smashes scissors.";
                     break;
                 case "paper":
-                    computer === "scissors" ? result = "You lose! Scissors cutss paper." : result = "You win! Paper covers rock.";
+                    computer === "scissors" ? result.textContent = "You lose! Scissors cutss paper." : result.textContent = "You win! Paper covers rock.";
                     break;
                 case "scissors":
-                    computer === "rock" ? result = "You lose! Rock smashes scissors." : result = "You win! Scissors cuts paper.";
+                    computer === "rock" ? result.textContent = "You lose! Rock smashes scissors." : result.textContent = "You win! Scissors cuts paper.";
                     break;
                 default:
                     return "error";
             }
         }
 
-        result.includes("win") ? playerScore++ : result.includes("lose") ? computerScore++ : null;
-        console.log(result);
-        console.log(`Score: Player - ${playerScore} | Computer - ${computerScore}`)
+        result.textContent.includes("win") ? playerScore++ : result.textContent.includes("lose") ? computerScore++ : null;
+        score.textContent = `Score: Player - ${playerScore} | Computer - ${computerScore}`;
         if (playerScore === 5) {
-            console.log("You win the game!");
+            game_screen.classList.toggle("hidden");
+            game_screen.classList.toggle("visible");
+            end_screen.classList.toggle("hidden");
+            end_screen.classList.toggle("visible");
+            end_text.textContent = "You win the game!";
         } else if (computerScore === 5) {
-            console.log("Computer wins the game! Try again?")
+            game_screen.classList.toggle("hidden");
+            game_screen.classList.toggle("visible");
+            end_screen.classList.toggle("hidden");
+            end_screen.classList.toggle("visible");
+            end_text.textContent = "Computer wins the game! Try again?";
         }
     }
+
+    again.addEventListener("click", () => {
+        result.textContent = "";
+        score.textContent = "";
+        playerScore = 0;
+        computerScore = 0;
+        end_screen.classList.toggle("hidden");
+        end_screen.classList.toggle("visible");
+        game_screen.classList.toggle("hidden");
+        game_screen.classList.toggle("visible");
+    })
 });
